e1d55f16a7bc3242dfa498eaef1f165dfe4d7ff5
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-26 23:45:44 +0200

    Add project files.

---
+++ MVCDayOne/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using MVCDayOne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDayOne.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // Ways to Send data
+        //Route Data in url ? 
+        //Post have body so can cary data // Form Data
+        //Get doesn't hava body so data sent in URl // Query String 
+        //priorty 1 Form , 2 Route , 3 Query // send data from view to controller
+        //model binder allows mapping HTTP request data with a model
+
+
+        public ViewResult index()
+        {
+            return View();
+        }
+        public ViewResult add(Student s)
+        {
+            #region
+            //int idQueryString = int.Parse(Request.QueryString["id"]);
+            //int idRout = int.Parse(RouteData.Values["id"].ToString());
+            //int idForm = int.Parse(Request.Form["id"]); 
+            #endregion
+
+            //viewBag store data like dynamic dictionary
+            ViewBag.id = s.Id;
+            ViewBag.name = s.Name;
+            ViewBag.age = s.Age;
+
+            return View();
+        }
+
+    }
+}
---
+++ MVCDayOne/Controllers/testController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDayOne.Controllers
+{
+    public class testController : Controller
+    {
+       public string home(int id , string name )
+        {
+            return "I am Saber" + id.ToString() + name;
+        }
+        public string Index()
+        {
+            return "Index";
+        }
+
+        public ViewResult about()
+        {
+            return View();
+        }
+
+        public ViewResult contact()
+        {
+            return View();
+        }
+
+    }
+}
