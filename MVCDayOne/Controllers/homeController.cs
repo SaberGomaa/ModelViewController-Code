6aec80d0b2467c48798b4493331bdd294b5138fe
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-27 13:59:45 +0200

    ViewBag VS ViewData

--- MVCDayOne/Controllers/homeController.cs
+++ MVCDayOne/Controllers/homeController.cs
@@ -14,12 +14,25 @@ namespace MVCDayOne.Controllers
             return View();
         }
 
+        #region MyRegion
         //post verb
         [HttpPost] // when form method is post 
-        public ViewResult index(int id , string name , int age)
+        public ViewResult index(int id, string name, int age)
         {
             return View();
         }
 
+        #endregion
+
+        public ViewResult add(int id, string name, int age)
+        {
+            // Dictionary
+
+            ViewData["id"] = id;
+            ViewData["name"] = name;
+            ViewData["age"] = age;
+
+            return View();
+        }
     }
 }
