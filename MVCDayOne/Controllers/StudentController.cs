f6420229f5f9032e5f4d6140a5bf64d3aa31ee16
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-27 10:57:27 +0200

    verb post and get

--- MVCDayOne/Controllers/StudentController.cs
+++ MVCDayOne/Controllers/StudentController.cs
@@ -14,8 +14,9 @@ namespace MVCDayOne.Controllers
         //Route Data in url ? 
         //Post have body so can cary data // Form Data
         //Get doesn't hava body so data sent in URl // Query String 
-        //priorty 1 Form , 2 Route , 3 Query // send data from view to controller
+        //priorty 1 FormData , 2 RouteData , 3 Querystring // send data from view to controller
         //model binder allows mapping HTTP request data with a model
+
 
         ITIContext context = new ITIContext();
         public ViewResult index()
@@ -30,7 +31,7 @@ namespace MVCDayOne.Controllers
             //int idForm = int.Parse(Request.Form["id"]); 
             #endregion
 
-            //viewBag store data like dynamic dictionary
+            //viewBag store data like dynamic dictionary => key and value 
             ViewBag.id = s.Id;
             ViewBag.name = s.Name;
             ViewBag.age = s.Age;
---
+++ MVCDayOne/Controllers/homeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDayOne.Controllers
+{
+    public class homeController : Controller
+    {
+        // Get verb
+        public ViewResult index()
+        {
+            return View();
+        }
+
+        //post verb
+        [HttpPost] // when form method is post 
+        public ViewResult index(int id , string name , int age)
+        {
+            return View();
+        }
+
+    }
+}
